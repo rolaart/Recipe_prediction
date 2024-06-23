717dd6311ebc7c3a75cd097b87df5b56b9ccca2a
Ivelina Balcheva <44026480+rolaart@users.noreply.github.com>
2024-06-23 16:58:19 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,13 +1,39 @@
 import pickle
+import pandas as pd
+from datetime import datetime, timedelta
 
 def load_model():
-    # Зареждане на предварително обучен модел от файл
+    # Примерен модел - в реален сценарий може да бъде по-сложен модел за препоръки
     with open('model.pkl', 'rb') as f:
         model = pickle.load(f)
     return model
 
-def predict(model, input_data):
-    # Предсказване на резултатите с модела
-    # Очакваме input_data да е в подходящ формат (например списък)
-    result = model.predict([input_data])
-    return result.tolist()
+def predict(model, recipes):
+    today = datetime.now()
+    season = get_season(today)
+    
+    # Филтриране на рецепти, които не са приготвяни дълго време и са в сезона
+    filtered_recipes = []
+    for recipe in recipes:
+        last_cooked_date = datetime.strptime(recipe['last_cooked_date'], '%Y-%m-%d')
+        if 'season' in recipe and recipe['season'] != season:
+            continue
+        days_since_last_cooked = (today - last_cooked_date).days
+        filtered_recipes.append((days_since_last_cooked, recipe))
+    
+    # Сортиране по най-дълго време неприготвяни
+    filtered_recipes.sort(reverse=True, key=lambda x: x[0])
+    
+    # Връщане на сортиран списък от препоръчани рецепти
+    return [recipe[1] for recipe in filtered_recipes]
+
+def get_season(date):
+    month = date.month
+    if month in [12, 1, 2]:
+        return 'winter'
+    elif month in [3, 4, 5]:
+        return 'spring'
+    elif month in [6, 7, 8]:
+        return 'summer'
+    else:
+        return 'fall'
